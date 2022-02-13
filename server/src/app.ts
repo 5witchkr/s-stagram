586b934af846b4ad1f231a33796d67c93a6dc721
5witchkr <hob4410@gmail.com>
2022-02-13 14:22:08 +0900

    2번

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('build'));//build파일 접근 허용
 
 app.get('/', function (req,res) {
     res.sendFile(__dirname + '/build/index.html');//__dirname은 루트경로알려주는것?
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('server is running...');
 });
